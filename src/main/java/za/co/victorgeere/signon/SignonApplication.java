27a5ac771b5c31b5c508d5aad887702b255256d1
Victor Geere <victor.geere@smsportal.com>
2018-11-16 02:32:26 +0200

    spring boot init

---
+++ src/main/java/za/co/victorgeere/signon/SignonApplication.java
@@ -0,0 +1,12 @@
+package za.co.victorgeere.signon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SignonApplication.class, args);
+    }
+}

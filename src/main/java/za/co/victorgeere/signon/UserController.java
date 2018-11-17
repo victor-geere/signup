bd8cbffeb565f4a67c802ad9d5b5f908d4df6b86
Victor Geere <victor.geere@smsportal.com>
2018-11-17 08:07:38 +0200

    add repository for /users endpoint

--- src/main/java/za/co/victorgeere/signon/UserController.java
+++ src/main/java/za/co/victorgeere/signon/UserController.java
@@ -3,21 +3,23 @@ package za.co.victorgeere.signon;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Optional;
 
 @Controller
+@RequestMapping("/api")
 public class UserController {
 
-    @GetMapping("/users")
+    @GetMapping("/user")
     @ResponseBody
     @Bean
-    public List<User> getUser(UserRepository repository) {
+    public User getUser() {
 
-        return repository.findByUsername("guest");
+//        return repository.findByUsername("guest");
 
-//        return new User("guest", "000-000-0000", "");
+        return new User("guest", "000-000-0000", "");
     }
 }

358fb4c9b6af6df732b2d3d65ae985d69b969b3c
Victor Geere <victorgeere@gmail.com>
2018-11-19 13:48:30 +0200

    add security config

---
+++ src/main/java/za/co/victorgeere/signon/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package za.co.victorgeere.signon.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                    .antMatchers("/**").permitAll()
+                .and()
+                    .csrf().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("asdf").roles("USER")
+                .and()
+                .withUser("admin").password("asdf").roles("ADMIN", "USER");
+    }
+}

eb48b4ec6bf00b06589a84c3d88fdc8dbae2871a
Victor Geere <victor.geere@smsportal.com>
2018-11-22 00:13:00 +0200

    fix security

--- src/main/java/za/co/victorgeere/signon/config/SecurityConfig.java
+++ src/main/java/za/co/victorgeere/signon/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package za.co.victorgeere.signon.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,31 +12,38 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import za.co.victorgeere.signon.user.H2UserDetailsService;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Autowired
-    H2UserDetailsService userDetailsService;
+    public static class SecureApiConfiguratorAdapter extends WebSecurityConfigurerAdapter {
+        @Autowired
+        H2UserDetailsService userDetailsService;
 
-    @Override
-    public void configure(AuthenticationManagerBuilder builder) throws Exception {
-        builder.userDetailsService(userDetailsService);
-    }
+        @Override
+        public void configure(AuthenticationManagerBuilder builder) throws Exception {
+            builder.userDetailsService(userDetailsService);
+        }
 
-    @Override
-    public void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.httpBasic().and().authorizeRequests().anyRequest().authenticated()
-                .antMatchers("/api/users/**").hasRole("USER")
-                .antMatchers("/h2/**").permitAll()
-                .and()
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().antMatchers(HttpMethod.PUT,"/api/users").permitAll().and().csrf().disable();
+            http.authorizeRequests().antMatchers(HttpMethod.POST,"/api/logout").permitAll().and().csrf().disable();
+
+            http.httpBasic().and().authorizeRequests().anyRequest().authenticated()
+                    .antMatchers(HttpMethod.GET,"/api/users/**").hasRole("USER").and().csrf().disable();
+
+            http.authorizeRequests().antMatchers("/h2/**").permitAll()
+                    .and()
                     .csrf().ignoringAntMatchers("/h2/**")
-                .and()
+                    .and()
                     .headers().frameOptions().sameOrigin()
-                .and()
+                    .and()
                     .csrf().disable();
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder(12);
+        }
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
-    }
 }
